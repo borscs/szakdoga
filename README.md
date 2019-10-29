fdbdaed0d83315047a906839376f8150d744a53a
Borscs Andras <borscs.andras@promera.hu>
2019-10-29 11:15:07 +0100

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# szakdoga
-# szakdoga
+# Online-Compiler
+
+This is an online compiler that can compile and run C\C++  Program. 
+
+# Languages
+C , C++ 
+
+# Requirements
+
+Linux, gcc, g++,
+
+
+# Change Permission
+```
+chmod -R 777 Online-Compiler
+``` 
--- php/com/compile.php
+++ php/com/compile.php
@@ -20,21 +20,6 @@
 					include("../compilers/cpp.php");
 					break;
 				}
-				case "java":
-				{	
-					include("../compilers/java.php");
-					break;
-				}
-				case "python2.7":
-				{
-					include("../compilers/python27.php");
-					break;
-				}
-				case "python3.2":
-				{
-					include("../compilers/python32.php");
-					break;
-				}
 			}
 	}
 ?>
